7672c47e542990b3238dbeb2ddbc48baace27fb9
mmbadcom <60084809+mmbadcom@users.noreply.github.com>
2022-01-15 22:53:35 +0800

    Create new.js

---
+++ new.js
@@ -0,0 +1,18 @@
+最新地址發布：
+i996.cc u996.cc z996.cc
+
+最新可訪問地址：
+m.doglive.xyz
+m.dogtop.xyz
+
+永久地址获取邮箱：
+mmbadcom@gmail.com
+
+永久訪問地址：
+fuckbe.com
+javmm.com
+mmbad.com
+
+後宮在線影視：
+abmov.icu
+hougong.me
